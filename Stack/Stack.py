15c062ce5e7c3103b4cab9b87835fe870f6d5c9d
Sancheeta Kaushal <sk11juit@gmail.com>
2014-09-28 22:10:11 +0530

    Create Stack.py

---
+++ Stack/Stack.py
@@ -0,0 +1,20 @@
+#code for stack in object oriented python 
+class Stack:
+    def __init__(self):
+        self.items=[];
+    def isEmpty(self):
+        return self.items==[]
+    def push(self,item):
+        self.items.append(item);
+    def pop(self):
+        if self.items is not None:
+            return self.items.pop();
+        else:
+            return None
+    def peek(self):
+        return self.items[len(self.items)-1];
+    def size(self):
+        return len(self.items);
+    def display(self):
+        for each in self.items:
+            print each       
